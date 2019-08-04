190dd66619de74f29408c2e109ee9a4a31ff53c1
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-08-04 17:42:27 +0500

    Initial Commit

---
+++ RuuviTag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RuuviTag
+//
+//  Created by Rinat Enikeev on 8/4/19.
+//  Copyright © 2019 Rinat Enikeev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
